c70083e7306e4c6bf7d2c33471c7854407f56d2b
Ryan Wicks <ryancwicks@gmail.com>
2018-01-16 22:50:32 -0500

    Set up project to better, and included gtest as a cmake fetched external.

---
+++ ProjectFiles.cmake
@@ -0,0 +1,23 @@
+# Copyright (c) 2018 Ryan Wicks (ryancwicks@gmail.com)
+# Distributed under the MIT License.
+# See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
+
+# Set project source files.
+set(SRC
+  "${SRC_PATH}/PointSorter.cpp"
+)
+
+set (HEADER
+    "${INCLUDE_PATH}/SpatialPointSorter/PointSorter.hpp"
+)
+
+# Set project main file.
+set(MAIN_SRC
+  #"${SRC_PATH}/main.cpp"
+)
+
+# Set project test source files.
+set(TEST_SRC
+  "${TEST_SRC_PATH}/testPointSorter.cpp"
+)
+
